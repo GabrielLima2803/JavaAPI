31ad155e1ba9bb9bdef29c182712d155816fe2d6
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-06 16:23:34 -0300

    Fix UserService, taskRepository Create

--- src/main/java/com/gabriellima/todosimple/services/UserService.java
+++ src/main/java/com/gabriellima/todosimple/services/UserService.java
@@ -14,8 +14,7 @@ import com.gabriellima.todosimple.repositories.UserRepository;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private TaskRepository taskRepository;
+
 
     public User findById(Long id){
         Optional<User> user = this.userRepository.findById(id);
@@ -28,7 +27,6 @@ public class UserService {
     public User create(User obj){
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTask());
         return obj;
     }
 
